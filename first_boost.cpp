cd68fef9c85dfe688637a8d661d6487f0715694b
boxgf <prsiddharthan92@gmail.com>
2016-05-25 09:18:27 -0400

    slot return value of latest connect

--- first_boost.cpp
+++ first_boost.cpp
@@ -15,7 +15,7 @@ struct World
     std::cout << "world !!" << std::endl;
   }
 };
-
+/*
 void print_args(float x, float y)
 {
   std::cout << "The arguments are " << x << " and " << y << std::endl;
@@ -40,25 +40,31 @@ void print_quotient(float x, float y)
 {
   std::cout << "The quotient is " << x / y << std::endl;
 }
+*/
+
+float product(float x, float y) { return x * y; }
+float quotient(float x, float y) { return x / y; }
+float sum(float x, float y) { return x + y; }
+float difference(float x, float y) { return x - y; }
 
 int main()
 {
 
  // Signal with no arguments and a void return value
-  boost::signals2::signal<void (float,float)> sig;          // create a signalm sig with void return type and no input parameters
+  boost::signals2::signal<float (float,float)> sig; // create a signalm sig with void return type and no input parameters
 
   // Connect a HelloWorld slot
-  //HelloWorld hello;                              // hello is a function object
-  //sig.connect(100,Hello());                            // connect te signal to hello fn object
+  //HelloWorld hello;                               // hello is a function object
+  //sig.connect(100,Hello());                       // connect te signal to hello fn object
   //sig.connect(World(),boost::signals2::at_front);
-  sig.connect(&print_args);
-  sig.connect(&print_sum);
-  sig.connect(&print_product);
-  sig.connect(&print_difference);
-  sig.connect(&print_quotient);
+  //sig.connect(&print_args);
+  sig.connect(&sum);
+  sig.connect(&product);
+  sig.connect(&difference);
+  sig.connect(&quotient);                           // use groups to reorder the sequnce the slots are called. Otherwise they are default ordered the way you connect
+                                                    // ungrouped are called at end by default
 
-  sig(5., 3.);
-
+   std::cout << *sig(5, 3) << std::endl;
   // Call all of the slots
 
 
